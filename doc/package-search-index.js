75b9536ef03e8937c5671004cb8fd89372cd27a1
Haoren <LIMH0192@E.NTU.EDU.SG>
2024-11-18 13:53:34 +0800

    JavaDoc + UML Class diagram

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Application"},{"l":"AppointmentManagement"},{"l":"DataManagement"},{"l":"InventoryManagement"},{"l":"UserManagement"}];updateSearchResults();
